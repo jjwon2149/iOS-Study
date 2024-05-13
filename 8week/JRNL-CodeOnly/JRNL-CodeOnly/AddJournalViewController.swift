7eac3b8129bd8933f0c0490e3fa02b051273a016
jjwon2149 <jjwon2149@gmail.com>
2024-05-13 16:44:49 +0900

    feat: add New Entry input component

--- 8week/JRNL-CodeOnly/JRNL-CodeOnly/AddJournalViewController.swift
+++ 8week/JRNL-CodeOnly/JRNL-CodeOnly/AddJournalViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class AddJournalViewController: UIViewController {
-
+    
     private lazy var mainContainer: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -39,9 +39,27 @@ class AddJournalViewController: UIViewController {
         return stackView
     }()
     
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Journal Title"
+        return textField
+    }()
+    
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Journal Body"
+        return textView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "face.smiling")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         navigationItem.title = "New Entry"
         view.backgroundColor = .white
         
@@ -55,11 +73,19 @@ class AddJournalViewController: UIViewController {
         
         mainContainer.addArrangedSubview(ratingView)
         mainContainer.addArrangedSubview(toggleView)
+        mainContainer.addArrangedSubview(titleTextField)
+        mainContainer.addArrangedSubview(bodyTextView)
+        mainContainer.addArrangedSubview(imageView)
         
         view.addSubview(mainContainer)
         
         let safeArea = view.safeAreaLayoutGuide
         mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        ratingView.translatesAutoresizingMaskIntoConstraints = false
+        toggleView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
         
         NSLayoutConstraint.activate([
@@ -68,7 +94,18 @@ class AddJournalViewController: UIViewController {
             mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
             
             ratingView.widthAnchor.constraint(equalToConstant: 252),
-            ratingView.heightAnchor.constraint(equalToConstant: 44)
+            ratingView.heightAnchor.constraint(equalToConstant: 44),
+            
+            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            
+            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
+            
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+
         ])
         
         
@@ -83,13 +120,13 @@ class AddJournalViewController: UIViewController {
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
