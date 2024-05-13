cca0ac8708143c5c3a9f8fb9bff3331ec198ee11
jjwon2149 <jjwon2149@gmail.com>
2024-05-13 09:12:16 +0900

    initial commit

---
+++ 8week/JRNL-CodeOnly/JRNL-CodeOnly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL-CodeOnly
+//
+//  Created by 정종원 on 5/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
