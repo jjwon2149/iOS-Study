fcc934feda06a407d0c0469382e2b7a72a0f6394
jjwon2149 <jjwon2149@gmail.com>
2024-05-14 13:30:10 +0900

    feat: add Location update code

--- 8week/JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
+++ 8week/JRNL/JRNL/Add New Entry Screen/AddJournalEntryViewController.swift
@@ -15,8 +15,8 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
     @IBOutlet var photoImageView: UIImageView!
     @IBOutlet var saveButton: UIBarButtonItem!
     
-    @IBOutlet var getLocationLocationSwitch: UISwitch!
-    @IBOutlet var getLocationLocationSwitchLabel: UILabel!
+    @IBOutlet var getLocationSwitch: UISwitch!
+    @IBOutlet var getLocationSwitchLabel: UILabel!
     
     
     var newJournalEntry: JournalEntry?
@@ -44,8 +44,10 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         let body = bodyTextView.text ?? ""
         let photo = photoImageView.image
         let rating = 3
+        let lat = currentLocation?.coordinate.latitude
+        let lon = currentLocation?.coordinate.longitude
         newJournalEntry = JournalEntry(rating: rating, title: title,
-                                       body: body, photo: photo)
+                                       body: body, photo: photo, latitude: lat, longitude: lon)
     }
 
     // MARK: - UITextFieldDelegate
@@ -78,10 +80,37 @@ class AddJournalEntryViewController: UIViewController, UITextFieldDelegate, UITe
         updateSaveButtonState()
     }
     
+    //MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            currentLocation = myCurrentLocation
+            getLocationSwitchLabel.text = "Done"
+            updateSaveButtonState()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("failed to find user's location: \(error.localizedDescription)")
+    }
+    
     // MARK: - Methods
     private func updateSaveButtonState() {
         let textFieldText = titleTextField.text ?? ""
         let textViewText = bodyTextView.text ?? ""
-        saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        if getLocationSwitch.isOn {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty && currentLocation != nil
+        } else {
+            saveButton.isEnabled = !textFieldText.isEmpty && !textViewText.isEmpty
+        }
+    }
+    
+    @IBAction func getLocationSwitchValueChanged(_ sender: UISwitch) {
+        if getLocationSwitch.isOn {
+            getLocationSwitchLabel.text = "Getting location..."
+            locationManager.requestLocation()
+        } else {
+            currentLocation = nil
+            getLocationSwitchLabel.text = "Get location"
+        }
     }
 }
