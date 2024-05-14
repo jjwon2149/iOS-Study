0fefde71721159fc2319064522003dedf9e68c5c
jjwon2149 <jjwon2149@gmail.com>
2024-05-14 14:14:09 +0900

    feat: add MapViewController

---
+++ 8week/JRNL/JRNL/MapScreen/MapViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapViewController.swift
+//  JRNL
+//
+//  Created by 정종원 on 5/14/24.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        self.navigationItem.title = "Loading..."
+        locationManager.requestLocation()
+    }
+    
+    //MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myLocation = locations.first {
+            let lat = myLocation.coordinate.latitude
+            let lon = myLocation.coordinate.longitude
+            self.navigationItem.title = "Map"
+            mapView.region = setInitialRegion(lat: lat, lon: lon)
+        }
+    }
+    
+    //MARK: - Methods
+    func setInitialRegion(lat: CLLocationDegrees, lon: CLLocationDegrees) -> MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)) //중앙의 범위
+    }
+}
