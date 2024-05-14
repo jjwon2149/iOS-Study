22699dd4b1a6b308d52f529de80119e354507bf8
jjwon2149 <jjwon2149@gmail.com>
2024-05-14 12:48:00 +0900

    feat: add DetailView Method, width,height setting

--- 8week/JRNL-CodeOnly/JRNL-CodeOnly/JournalDetailViewController.swift
+++ 8week/JRNL-CodeOnly/JRNL-CodeOnly/JournalDetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class JournalDetailViewController: UIViewController {
+class JournalDetailViewController: UITableViewController {
     
     let journalEntry: JournalEntry
     
@@ -23,19 +23,33 @@ class JournalDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         navigationItem.title = "Detail"
         // Do any additional setup after loading the view.
     }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 1: return 60
+        case 3: return 150
+        case 4: return 316
+        case 5: return 316
+        default: return 44.5
+        }
+    }
 }
