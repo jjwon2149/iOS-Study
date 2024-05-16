3f739161b2c6d258c6feb3da8420c13168700310
jjwon2149 <jjwon2149@gmail.com>
2024-05-16 13:21:18 +0900

    feat: add RatingView

---
+++ 8week/JRNL/JRNL/View/RatingView.swift
@@ -0,0 +1,46 @@
+//
+//  RatingView.swift
+//  JRNL
+//
+//  Created by 정종원 on 5/16/24.
+//
+
+import UIKit
+
+class RatingView: UIStackView {
+    private var ratingButtons = [UIButton()]
+    var rating = 0
+    private let buttonSize = CGSize(width: 44.0, height: 44.0)
+    private let buttonCount = 6
+    
+    //MARK: - Initialization
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    //MARK: - Private Method
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        let filledStar = UIImage(systemName: "star.fill")
+        let emptyStar = UIImage(systemName: "star")
+        let highlightedStar = UIImage(systemName: "star.fill")?
+            .withTintColor(.red, renderingMode: .alwaysOriginal)
+        
+        for _ in 0 ..< buttonCount {
+            let button = UIButton()
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.widthAnchor.constraint(equalToConstant: buttonSize.width).isActive = true
+            button.heightAnchor.constraint(equalToConstant: buttonSize.height).isActive = true
+            addArrangedSubview(button)
+            ratingButtons.append(contentsOf: button)
+        }
+    }
+}
