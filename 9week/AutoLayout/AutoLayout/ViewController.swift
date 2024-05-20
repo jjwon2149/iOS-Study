20ccaf7e5ea881ad3ccecc3560870e0f64e2a851
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 10:01:31 +0900

    feat: top,leading,trailing,bottom 제약조건 추가

--- 9week/AutoLayout/AutoLayout/ViewController.swift
+++ 9week/AutoLayout/AutoLayout/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,16 +17,25 @@ class ViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(label)
         
-//        NSLayoutConstraint.activate([
-//            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-//        ])
-            //OR 같은 코드.
+        //        NSLayoutConstraint.activate([
+        //            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        //        ])
+        //OR 같은 코드.
         label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = false
         label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = false
+        
+        //화면 상단 상태바나 하단 홈 핸들에 겹치지 않는 영역으로 위치시킬때
         label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        //      제약조건 하나하나 isActive 프로퍼티 설정하는 대신, activate 함수로 한번에 활성화 할 수 있다.
+        //        NSLayoutConstraint.activate([
+        //            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
     }
-
-
+    
+    
 }
 
