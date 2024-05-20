c2a499d4cbbc450b108128facdd40fbfcc29b502
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 11:45:17 +0900

    feat: add UISwitch in codebase UIKit

--- 9week/StackView/StackView/ViewController.swift
+++ 9week/StackView/StackView/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let toggleSwitch = UISwitch()
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        label.text = "Switch is OFF"
+        label.textAlignment = .center
         
+        toggleSwitch.addTarget(self, action: #selector(switchChanged), for: .valueChanged)
         
+        let stackView = UIStackView(arrangedSubviews: [label, toggleSwitch])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+    }
+}
+
+extension ViewController {
+    @objc func switchChanged(_ sender: UISwitch) {
+        if sender.isOn {
+            label.text = "Switch is ON"
+        } else {
+            label.text = "Switch is OFF"
+        }
     }
 }
 
