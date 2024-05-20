b1feb05c179e6d24667f069c7c011efabf633027
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 16:28:55 +0900

    Clear

--- 9week/TextProject in codebase UIKit/TextProject in codebase UIKit/ViewController.swift
+++ 9week/TextProject in codebase UIKit/TextProject in codebase UIKit/ViewController.swift
@@ -13,49 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let htmlString = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <style>
-                body {
-                    font-family: -apple-system, Helvetica, Arial, sans-serif;
-                    font-size: 2rem;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                .italic {
-                    font-style: italic;
-                }
-                .blue {
-                    color: blue;
-                }
-                .underline {
-                    text-decoration: underline;
-                }
-                
-            </style>
-        </head>
-        <body>
-            <span class="italic underline">Hello,&nbsp;</span><span class="blue underline">World</span>
-        </body>
-        </html>
-        """
-        let webView = WKWebView()
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        
-        webView.loadHTMLString(htmlString, baseURL: nil)
-        view.addSubview(webView)
-        
-        NSLayoutConstraint.activate([
-            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-        
     }
     
     
