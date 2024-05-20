f9ae45fc0a001a9def9255ed0f1f974b0567904d
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 13:29:18 +0900

    feat: add UIStepper

--- 9week/StackView/StackView/ViewController.swift
+++ 9week/StackView/StackView/ViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let toggleSwitch = UISwitch()
+    let stepper = UIStepper()
     let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        label.text = "값: 0"
         
+        stepper.minimumValue = 0
+        stepper.maximumValue = 10
+        stepper.stepValue = 1
+        stepper.value = 0
+        stepper.addAction(UIAction{ [weak self] _ in
+            self?.label.text = "값: \(Int(self?.stepper.value ?? 0))"
+        }, for: .valueChanged)
+        
+        let stackView = UIStackView(arrangedSubviews: [label, stepper])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
