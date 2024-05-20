7366a937e42c3bc282be7e758db5a51a67103181
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 17:33:27 +0900

    feat: compare fit, fill

--- 9week/Image in codebase UIKit/Image in codebase UIKit/ViewController.swift
+++ 9week/Image in codebase UIKit/Image in codebase UIKit/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //이미지뷰는 이미지를 띄우는 액자~
+        let imageView = UIImageView(image: UIImage(systemName: "hare.fill"))
+        imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 50)
+        imageView.tintColor = .purple
+        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView)
         
+        let flagImageView = UIImageView(image: UIImage(systemName: "flag.fill"))
+        flagImageView.frame = CGRect(x: 50, y: 150, width: 100, height: 50)
+        flagImageView.clipsToBounds = true
+        flagImageView.tintColor = .blue
+        flagImageView.contentMode = .scaleAspectFill
+        view.addSubview(flagImageView)
     }
     
     
