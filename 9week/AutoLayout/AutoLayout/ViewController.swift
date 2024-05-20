0b640a5c359004cf073fa486c69ed350a1dfbb82
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 10:07:58 +0900

    feat: add 최소, 중간, 최대값 제약조건

--- 9week/AutoLayout/AutoLayout/ViewController.swift
+++ 9week/AutoLayout/AutoLayout/ViewController.swift
@@ -12,6 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let box = UIView()
+        box.backgroundColor = .red
+        box.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(box)
+        
+        NSLayoutConstraint.activate([
+            //박스안의 동적으로 바뀌는 값의 최소,최대값정의
+            box.widthAnchor.constraint(lessThanOrEqualToConstant: 200), //이것보단 같거나 커야함
+            box.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
+            
+            box.widthAnchor.constraint(equalToConstant: 1000),
+            box.heightAnchor.constraint(equalToConstant: 50),
+            
+            box.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            box.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            
+            
+            
+        ])
         
     }
     
