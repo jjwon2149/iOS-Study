344f473d3aa8157f4f5c0af34319a4b2f2f39c20
jjwon2149 <jjwon2149@gmail.com>
2024-05-20 15:28:01 +0900

    feat: add text with cornerRadius, clipsToBounds

--- 9week/TextProject in codebase UIKit/TextProject in codebase UIKit/ViewController.swift
+++ 9week/TextProject in codebase UIKit/TextProject in codebase UIKit/ViewController.swift
@@ -13,20 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let label = UILabel()
-        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas tellus rutrum tellus pellentesque eu. Nibh tortor id aliquet lectus proin nibh nisl condimentum id. Eget velit aliquet sagittis id consectetur. Aliquam eleifend mi in nulla posuere. Nunc consequat interdum varius sit amet mattis. Ac tincidunt vitae semper quis. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Id velit ut tortor pretium viverra. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Donec enim diam vulputate ut pharetra sit amet aliquam id."
-        label.textColor = .black
-        label.textAlignment = .left
-        label.font = UIFont.systemFont(ofSize: 17)
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
+        label.text = "stylized text"
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = .yellow
+        label.backgroundColor = .red
+        label.textAlignment = .center
+        
+        label.layer.cornerRadius = 10
+        label.clipsToBounds = true //subview들이 view의 bounds에 가둬질 수 있는 지를 판단하는 Boolean 값
+        
         label.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(label)
         
         NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            label.widthAnchor.constraint(equalToConstant: 200),
+            label.heightAnchor.constraint(equalToConstant: 40)
             
         ])
         
