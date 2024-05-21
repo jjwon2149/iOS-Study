151349a1670d5bab65777265eb2ba3e2a3322ee9
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 16:37:57 +0900

    feat: add UIAlertController

--- 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
+++ 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
@@ -12,6 +12,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let menuButton = UIButton(type: .system)
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Options"
+        menuButton.configuration = configuration
+        menuButton.addAction(UIAction { [weak self] _ in
+            self?.showMenu()
+        }, for: .touchUpInside)
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(menuButton)
+        
+        NSLayoutConstraint.activate([
+            menuButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            menuButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func showMenu() {
+        let alert = UIAlertController(title: "Options", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Open", style: .default) { _ in
+            self.showMessage("Open chosen")
+        })
+        alert.addAction(UIAlertAction(title: "Close", style: .destructive))
+        present(alert, animated: true)
+    }
+    func showMessage(_ message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
     }
 }
 
