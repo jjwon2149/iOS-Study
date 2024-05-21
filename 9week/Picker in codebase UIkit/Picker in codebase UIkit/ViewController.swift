6730c31b52d69271a12c7b1768900745bffe77f0
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 14:01:55 +0900

    feat: add datePicker

--- 9week/Picker in codebase UIkit/Picker in codebase UIkit/ViewController.swift
+++ 9week/Picker in codebase UIkit/Picker in codebase UIkit/ViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
  
+    let datePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let today = Date()
+        var dateComponents = DateComponents()
+        dateComponents.year = 1
+        let oneYearFromNow = Calendar.current.date(byAdding: dateComponents, to: today)
+        
+        datePicker.minimumDate = today
+        datePicker.maximumDate = oneYearFromNow
+        
+        datePicker.addAction(UIAction { [weak self] _ in
+            print("sender \(String(describing: self?.datePicker.date.formatted()))")
+        }, for: .valueChanged)
+        
+        view.addSubview(datePicker)
+        
+        NSLayoutConstraint.activate([
+            datePicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
 }
