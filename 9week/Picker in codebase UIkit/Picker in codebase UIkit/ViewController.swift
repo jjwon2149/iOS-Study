8e412762fdaa4d2b4b0679024c4700fa5532659a
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 14:37:26 +0900

    feat: add UIColorPicker

--- 9week/Picker in codebase UIkit/Picker in codebase UIkit/ViewController.swift
+++ 9week/Picker in codebase UIkit/Picker in codebase UIkit/ViewController.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIColorPickerViewControllerDelegate {
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let button = UIButton()
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Color Picker"
+        configuration.cornerStyle = .capsule
+        button.configuration = configuration
+        
+        //unowned optional하지 않은 self... 확실할때만 사용 해야함.
+        button.addAction(UIAction{ [unowned self] _ in
+            let colorPicker = UIColorPickerViewController()
+            colorPicker.delegate = self
+            colorPicker.supportsAlpha = false
+            colorPicker.selectedColor = view.backgroundColor ?? .white
+            self.present(colorPicker, animated: true)
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
         
     }
     
