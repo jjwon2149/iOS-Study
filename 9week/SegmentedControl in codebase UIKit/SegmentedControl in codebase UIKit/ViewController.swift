858e867d1f95afde9403b444f9016dc58cfe4fd8
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 10:16:33 +0900

    feat: add SegmentedControl

--- 9week/SegmentedControl in codebase UIKit/SegmentedControl in codebase UIKit/ViewController.swift
+++ 9week/SegmentedControl in codebase UIKit/SegmentedControl in codebase UIKit/ViewController.swift
@@ -8,9 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let segementedControl = UISegmentedControl(items: ["Red", "Green", "Blue"])
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        segementedControl.selectedSegmentIndex = 0
+        segementedControl.addAction(UIAction { [weak self] _ in
+            switch self?.segementedControl.selectedSegmentIndex {
+            case 0:
+                self?.view.backgroundColor = .red
+            case 1:
+                self?.view.backgroundColor = .green
+            case 2:
+                self?.view.backgroundColor = .blue
+            default:
+                self?.view.backgroundColor = .white
+                break
+            }
+        }, for: .valueChanged)
+        
+        segementedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segementedControl)
+        
+        NSLayoutConstraint.activate([
+            segementedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            segementedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+        ])
     }
 }
 
