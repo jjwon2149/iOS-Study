8f2ddca95da109dfda57955f1c3a5c7b5528a096
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 13:02:49 +0900

    Initialize Picker Project

---
+++ 9week/Picker in codebase UIkit/Picker in codebase UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picker in codebase UIkit
+//
+//  Created by 정종원 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
