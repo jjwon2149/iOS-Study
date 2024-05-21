0da3f0607aed7885e4ee2ea7dd36f44d797251fc
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 15:14:20 +0900

    feat: Add button to open url

--- 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
+++ 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let linkButton = UIButton(type: .system)
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Apple"
+        linkButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        
+        linkButton.configuration = configuration
+        
+        linkButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        linkButton.addAction(UIAction{ _ in
+            if let url = URL(string: "https://www.apple.com") {
+                UIApplication.shared.open(url)
+            }
+        }, for: .touchUpInside)
+        
+        view.addSubview(linkButton)
+        
+        NSLayoutConstraint.activate([
+            linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
