d56bb610e46dd57203b4386f4d5a51c7813b7178
jjwon2149 <jjwon2149@gmail.com>
2024-05-21 16:00:15 +0900

    feat: add close button

--- 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
+++ 9week/Link in codebase UIkit/Link in codebase UIkit/ViewController.swift
@@ -10,12 +10,22 @@ import WebKit
 import SafariServices
 
 class ViewController: UIViewController {
-
+    
     private lazy var webView: WKWebView = {
         let webView = WKWebView(frame: .zero)
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.isHidden = true
+        webView.layer.cornerRadius = 20
         return webView
+    }()
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .close)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addAction(UIAction{ [weak self] _ in
+            self?.webView.isHidden = true
+        }, for: .touchUpInside)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -32,25 +42,29 @@ class ViewController: UIViewController {
         
         linkButton.addAction(UIAction{ [weak self] _ in
             if let url = URL(string: "https://www.apple.com") {
-//                UIApplication.shared.open(url)
-//                self?.openInWebView(url: url)
-                let safariViewController = SFSafariViewController(url: url)
-                //다양한 모달뷰의 형태로 띄우기
-                safariViewController.modalPresentationStyle = .pageSheet
-                self?.present(safariViewController, animated: true)
+                //                UIApplication.shared.open(url)
+                //                let safariViewController = SFSafariViewController(url: url)
+                //                //다양한 모달뷰의 형태로 띄우기
+                //                safariViewController.modalPresentationStyle = .pageSheet
+                //                self?.present(safariViewController, animated: true)
+                self?.openInWebView(url: url)
             }
         }, for: .touchUpInside)
         
         view.addSubview(linkButton)
+        webView.addSubview(closeButton)
         view.addSubview(webView)
+        
         
         NSLayoutConstraint.activate([
             linkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             linkButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            closeButton.topAnchor.constraint(equalTo: webView.topAnchor, constant: -30),
+            closeButton.trailingAnchor.constraint(equalTo: webView.trailingAnchor, constant: -10)
         ])
     }
     
@@ -59,6 +73,6 @@ class ViewController: UIViewController {
         webView.load(request)
         webView.isHidden = false
     }
- 
+    
 }
 
