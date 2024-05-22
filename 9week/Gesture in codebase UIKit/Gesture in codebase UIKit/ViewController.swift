4680b23b49a90b32ac34865cdcb99a3f480127c2
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 09:25:18 +0900

    feat: add TapGesture, UITapGestureRecognizer

--- 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
+++ 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let rectangle = UIView()
+        rectangle.backgroundColor = .yellow
+        rectangle.frame = CGRect(x: 100, y: 100, width: 275, height: 125)
+        rectangle.isUserInteractionEnabled = true //터치 이벤트를 사용 가능
+        
+        view.addSubview(rectangle)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.numberOfTapsRequired = 2 //2번 탭을 해야만 함
+        rectangle.addGestureRecognizer(tapGesture)
+        
     }
 
-
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        if let view = sender.view {
+            view.backgroundColor = (view.backgroundColor == UIColor.yellow) ? .red : .yellow
+        }
+    }
 }
 
