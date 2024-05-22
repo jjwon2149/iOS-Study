2c1ecfebf20405ed82a0f737acd0e1ab460e83c8
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 14:23:46 +0900

    feat: add tableView

--- 9week/List in codebase UIKit/List in codebase UIKit/ViewController.swift
+++ 9week/List in codebase UIKit/List in codebase UIKit/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    let items = ["고앵이", "멍멍이", "왈왈", "컹컹", "킁킁", "캥캥"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        view.addSubview(tableView)
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        var config = cell.defaultContentConfiguration()
+        config.text = items[indexPath.row]
+        cell.contentConfiguration = config
+        return cell
+    }
 
 }
 
