22ed20597baf037558b26fbcce5a06474937efc1
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 09:45:06 +0900

    feat: add UIPinchGestureRecognizer

--- 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
+++ 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
         
+        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
+        imageView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        imageView.isUserInteractionEnabled = true
         
+        view.addSubview(imageView)
+        
+        let gesture = UIPinchGestureRecognizer(target: self, action: #selector(handleGesture))
+        imageView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func handleGesture(_ sender: UIPinchGestureRecognizer) {
+        print(sender.scale)
+        if let view = sender.view {
+            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
+        }
     }
 
 }
