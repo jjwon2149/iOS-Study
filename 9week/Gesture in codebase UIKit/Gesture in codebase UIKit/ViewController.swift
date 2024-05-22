a14c055412f7217d8c61bcfc217016328965e79e
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 10:34:37 +0900

    feat: add UIRotationGestureRecognizer

--- 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
+++ 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
@@ -12,11 +12,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let imageView = UIImageView(image: UIImage(systemName: "star.fill"))
+        imageView.frame = CGRect(x: view.bounds.midX - 100, y: view.bounds.midY - 100, width: 200, height: 200)
+        imageView.isUserInteractionEnabled = true
         
+        let gesture = UIRotationGestureRecognizer(target: self, action: #selector(handleGesture))
+        
+        imageView.addGestureRecognizer(gesture)
+        
+        view.addSubview(imageView)
     }
     
-    @objc func handleGesture(_ sender: UIPinchGestureRecognizer) {
-        
+    @objc func handleGesture(_ sender: UIRotationGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.rotated(by: sender.rotation)
+            sender.rotation = 0
+        }
     }
     
 }
