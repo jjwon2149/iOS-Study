463279c8ade0a08167022c0dfa8bc2582339e2d8
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 09:38:05 +0900

    Clear

--- 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
+++ 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
@@ -11,26 +11,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+ 
         
-        
-        let rectangle = UIView()
-        rectangle.backgroundColor = .yellow
-        rectangle.frame = CGRect(x: 100, y: 100, width: 275, height: 125)
-        rectangle.isUserInteractionEnabled = true //터치 이벤트를 사용 가능
-        
-        view.addSubview(rectangle)
-        
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture))
-        gesture.minimumPressDuration = 2.0
-        rectangle.addGestureRecognizer(gesture)
         
     }
-    
-    @objc func handleGesture(_ sender: UILongPressGestureRecognizer) {
-        print("\(sender.state)")
-        if let view = sender.view, sender.state == .began {
-            view.backgroundColor = (view.backgroundColor == UIColor.yellow) ? .red : .yellow
-        }
-    }
+
 }
 
