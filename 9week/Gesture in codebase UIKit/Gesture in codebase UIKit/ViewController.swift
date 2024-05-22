fb01a9b4f5224547957e8aca1c12a438ab6f98b7
jjwon2149 <jjwon2149@gmail.com>
2024-05-22 11:14:12 +0900

    다중 제스처

--- 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
+++ 9week/Gesture in codebase UIKit/Gesture in codebase UIKit/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,10 +17,32 @@ class ViewController: UIViewController {
         imageView.isUserInteractionEnabled = true
         
         let gesture = UIRotationGestureRecognizer(target: self, action: #selector(handleGesture))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture))
         
         imageView.addGestureRecognizer(gesture)
+        imageView.addGestureRecognizer(panGesture)
+        imageView.addGestureRecognizer(pinchGesture)
+        
+        gesture.delegate = self
+        panGesture.delegate = self
+        pinchGesture.delegate = self
         
         view.addSubview(imageView)
+    }
+    
+    //단일 제스처 허용 여뷰
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UIPanGestureRecognizer {
+            return true
+        } else {
+            return true
+        }
+    }
+    
+    //복합 제스처 허용 여부
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
     @objc func handleGesture(_ sender: UIRotationGestureRecognizer) {
@@ -30,5 +52,18 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+        let tranlation = sender.translation(in: view)
+        if let view = sender.view {
+            view.center = CGPoint(x: view.center.x + tranlation.x, y: view.center.y + tranlation.y)
+            sender.setTranslation(.zero, in: view)
+        }
+    }
+    
+    @objc func handlePinchGesture(_ sender: UIPinchGestureRecognizer) {
+        if let view = sender.view {
+            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
+        }
+    }
 }
 
