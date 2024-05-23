b93d7e9a79e83118ca86baa97924da0854007154
jjwon2149 <jjwon2149@gmail.com>
2024-05-23 16:09:26 +0900

    feat: 키보드 높이에 따른 이벤트 처리

--- 9week/KeyboardInCodebaseUIKit/KeyboardInCodebaseUIKit/ViewController.swift
+++ 9week/KeyboardInCodebaseUIKit/KeyboardInCodebaseUIKit/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     let textField = UITextField()
     
     override func viewDidLoad() {
@@ -24,8 +25,37 @@ class ViewController: UIViewController {
             textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             textField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
         ])
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
+        view.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector:#selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
+    @objc func tapHandler(_ sender: UIView) {
+        textField.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        print("keyboard up")
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardSize.height
+            if view.frame.origin.y == 0 {
+                view.frame.origin.y -= keyboardHeight
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        print("keyboard down")
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
+    }
     
 }
-
