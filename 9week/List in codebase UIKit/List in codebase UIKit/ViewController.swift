2e3e4aa7630d14d3ea9a9107f8d2b065680cdcfa
jjwon2149 <jjwon2149@gmail.com>
2024-05-23 09:33:19 +0900

    feat: add tableView with image

--- 9week/List in codebase UIKit/List in codebase UIKit/ViewController.swift
+++ 9week/List in codebase UIKit/List in codebase UIKit/ViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
+struct Animal {
+    let name: String
+    let image: UIImage
+}
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let animals = [
+        Animal(name: "강아지", image: UIImage(systemName: "dog")!),
+        Animal(name: "고양이", image: UIImage(systemName: "cat")!),
+        Animal(name: "토끼", image: UIImage(systemName: "hare")!)
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
  
+        let tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+    }
+    
+    //MARK: - UITableViewDataSource Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = animals[indexPath.row].name
+        configuration.image = animals[indexPath.row].image
+        cell.contentConfiguration = configuration
+        return cell
     }
 }
-
