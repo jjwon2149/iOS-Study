258d4d589773762894d1d2cff81500dcf41a2dcb
jjwon2149 <jjwon2149@gmail.com>
2024-05-27 14:30:00 +0900

    refactor: update code

--- 8week/JRNL/JRNL/View/RatingView.swift
+++ 8week/JRNL/JRNL/View/RatingView.swift
@@ -8,22 +8,22 @@
 import UIKit
 
 class RatingView: UIStackView {
-    private var ratingButtons = [UIButton()]
-    var rating = 0{
+    private var ratingButtons: [UIButton] = []
+    var rating = 0 {
         didSet {
             updateButtonSelectionState()
         }
     }
     private let buttonSize = CGSize(width: 44.0, height: 44.0)
-    private let buttonCount = 6
+    private let buttonCount = 5
     
-    //MARK: - Initialization
+    // MARK: - Initialization
     required init(coder: NSCoder) {
         super.init(coder: coder)
         setupButtons()
     }
     
-    //MARK: - Private Method
+    // MARK: - Private Methods
     private func setupButtons() {
         for button in ratingButtons {
             removeArrangedSubview(button)
@@ -33,18 +33,19 @@ class RatingView: UIStackView {
         let filledStar = UIImage(systemName: "star.fill")
         let emptyStar = UIImage(systemName: "star")
         let highlightedStar = UIImage(systemName: "star.fill")?
-            .withTintColor(.red, renderingMode: .alwaysOriginal)
+            .withTintColor(.red,
+                           renderingMode: .alwaysOriginal)
         
-        for _ in 0 ..< buttonCount {
+        for _ in 0..<buttonCount {
             let button = UIButton()
             button.setImage(emptyStar, for: .normal)
             button.setImage(filledStar, for: .selected)
             button.setImage(highlightedStar, for: .highlighted)
             button.setImage(highlightedStar, for: [.highlighted, .selected])
             button.translatesAutoresizingMaskIntoConstraints = false
-//            button.widthAnchor.constraint(equalToConstant: buttonSize.width).isActive = true
-//            button.heightAnchor.constraint(equalToConstant: buttonSize.height).isActive = true
-            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchUpInside)
+            //            button.widthAnchor.constraint(equalToConstant: buttonSize.width).isActive = true
+            //            button.heightAnchor.constraint(equalToConstant: buttonSize.height).isActive = true
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchDown)
             addArrangedSubview(button)
             ratingButtons.append(button)
         }
@@ -58,7 +59,7 @@ class RatingView: UIStackView {
     
     @objc func ratingButtonTapped(button: UIButton) {
         guard let index = ratingButtons.firstIndex(of: button) else {
-            fatalError("The button. \(button), is not on the ratingButtons array: \(ratingButtons)")
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
         }
         let selectedRating = index + 1
         if selectedRating == rating {
