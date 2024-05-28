f5e470bf96ac66b61b13713fec351a711a65f30e
jjwon2149 <jjwon2149@gmail.com>
2024-05-28 13:09:37 +0900

    refactor: rename model

--- 8week/JRNL/JRNL/Model/JournalEntry.swift
+++ 8week/JRNL/JRNL/Model/JournalEntry.swift
@@ -12,23 +12,17 @@ import SwiftData
 @Model
 class JournalEntry {
     // MARK: - Properties
-    var key = UUID().uuidString
     let dateString: String
     let rating: Int
     let entryTitle: String
     let entryBody: String
-    let photoData: Data?
+    @Attribute(.externalStorage) let photoData: Data?
     let latitude: Double?
     let longitude: Double?
     
     // MARK: - Intialization
-    init?(rating: Int,
-          title: String,
-          body: String,
-          photo: UIImage? = nil,
-          latitude: Double? = nil,
-          longitude: Double? = nil
-    ){
+    init?(rating: Int, title: String, body: String,
+          photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
         if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
             return nil
         }
