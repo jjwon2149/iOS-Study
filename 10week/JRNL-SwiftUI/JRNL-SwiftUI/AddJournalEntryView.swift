efadeed23fc5063ed63863408f434ff4eb74ff9a
jjwon2149 <jjwon2149@gmail.com>
2024-05-28 17:29:44 +0900

    feat: add AddJournalEntryView Form

--- 10week/JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
+++ 10week/JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
@@ -10,10 +10,32 @@ import SwiftUI
 struct AddJournalEntryView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State private var isGetLocationOn = false
+    @State private var entryTitle = ""
+    @State private var entryBody = ""
     
     var body: some View {
         NavigationStack {
-            Text("Add Journal Entry")
+            
+            Form {
+                Section(header: Text("Rating")) {
+                    Rectangle()
+                        .foregroundStyle(.cyan)
+                }
+                Section(header: Text("Location")) {
+                    Toggle("Get Location", isOn: $isGetLocationOn)
+                }
+                Section(header: Text("Title")) {
+                    TextField("Enter title", text: $entryTitle)
+                }
+                Section(header: Text("Body")) {
+                    TextEditor(text: $entryBody)
+                }
+                Section(header: Text("Photo")) {
+                    Image(systemName: "face.smiling")
+                }
+            }
+            
                 .navigationTitle("Add Journal Entry")
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
