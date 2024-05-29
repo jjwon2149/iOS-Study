a5afb7f2ae0331972bf4678d0b6b7dbf10fa6c79
jjwon2149 <jjwon2149@gmail.com>
2024-05-29 13:06:26 +0900

    feat: 맵 어노테이션 콜아웃 추가

--- 10week/JRNL-SwiftUI/JRNL-SwiftUI/MapView.swift
+++ 10week/JRNL-SwiftUI/JRNL-SwiftUI/MapView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 import MapKit
 import CoreLocation
+import SwiftData
 
-struct mapUIView: UIViewRepresentable {
+struct MapUIView: UIViewRepresentable {
     
     
     @Binding var region: MKCoordinateRegion
     @Binding var annotations: [MKAnnotation]
-    @Binding var journalEntry: JournalEntry?
     @Binding var isDetailViewActive: Bool
     
     func makeUIView(context: Context) -> MKMapView {
@@ -34,12 +34,25 @@ struct mapUIView: UIViewRepresentable {
     }
     
     class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: mapUIView
-        init(_ parent: mapUIView) {
+        var parent: MapUIView
+        init(_ parent: MapUIView) {
             self.parent = parent
         }
         
         func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+            let identifier = "mapAnnotation"
+            if annotation is JournalMapAnnotation {
+                if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+                    annotationView.annotation = annotation
+                    return annotationView
+                } else {
+                    let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                    annotationView.canShowCallout = true
+                    let calloutButton = UIButton(type: .detailDisclosure)
+                    annotationView.rightCalloutAccessoryView = calloutButton
+                    return annotationView
+                }
+            }
             return nil
         }
     }
@@ -51,26 +64,27 @@ struct MapView: View {
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4149),
                                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     @State private var annotations: [MKAnnotation] = []
-    @State private var journalEntry: JournalEntry?
     @State private var isDetailViewActive = false
     
     @StateObject private var locationManager = LocationManager()
+    @Query(sort: \JournalEntry.date) var journalEntries: [JournalEntry]
+    
     
     var body: some View {
         NavigationStack {
-            mapUIView(region: $region, annotations: $annotations, journalEntry: $journalEntry, isDetailViewActive: $isDetailViewActive)
-                .onAppear {
-                    locationManager.requestLocation()
-                }
-                .onReceive(locationManager.$location) { location in
-                    if let location = location {
-                        region = MKCoordinateRegion(center: location.coordinate,
-                                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-                        annotations = []
-                    }
-                }
-                .navigationTitle("Map")
-                .navigationBarTitleDisplayMode(.inline)
+            MapUIView(region: $region, annotations: $annotations,
+                      isDetailViewActive: $isDetailViewActive)                .onAppear {
+                locationManager.requestLocation()
+            }
+                      .onReceive(locationManager.$location) { location in
+                          if let location = location {
+                              region = MKCoordinateRegion(center: location.coordinate,
+                                                          span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                              annotations = []
+                          }
+                      }
+                      .navigationTitle("Map")
+                      .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
