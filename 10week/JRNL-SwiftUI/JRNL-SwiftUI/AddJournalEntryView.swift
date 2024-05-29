1e38f7747f09ed64b3bd6828161ac8e1a18348f2
jjwon2149 <jjwon2149@gmail.com>
2024-05-29 09:16:35 +0900

    feat: add RatingView

--- 10week/JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
+++ 10week/JRNL-SwiftUI/JRNL-SwiftUI/AddJournalEntryView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+struct RatingView: View {
+    
+    @Binding var rating: Int
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            ForEach(0..<5) { index in
+                Image(systemName: index < rating ? "star.fill" : "star.")
+                    .foregroundStyle(.blue)
+                    .onTapGesture {
+                        rating = index + 1
+                    }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+    }
+}
+
 struct AddJournalEntryView: View {
     
     @Environment(\.modelContext) var modelContext
@@ -15,6 +33,7 @@ struct AddJournalEntryView: View {
     @State private var isGetLocationOn = false
     @State private var entryTitle = ""
     @State private var entryBody = ""
+    @State private var rating = 0
     
     var body: some View {
         NavigationStack {
@@ -22,7 +41,7 @@ struct AddJournalEntryView: View {
             Form {
                 Section(header: Text("Rating")) {
                     Rectangle()
-                        .foregroundStyle(.cyan)
+                        RatingView(rating: $rating)
                 }
                 Section(header: Text("Location")) {
                     Toggle("Get Location", isOn: $isGetLocationOn)
@@ -37,22 +56,21 @@ struct AddJournalEntryView: View {
                     Image(systemName: "face.smiling")
                 }
             }
-            
-                .navigationTitle("Add Journal Entry")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Button("Cancle") {
-                            dismiss()
-                        }
-                    }
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("Save") {
-                            let journalEntry = JournalEntry(rating: 3, entryTitle: entryTitle, entryBody: entryBody, latitude: nil, longitude: nil)
-                            dismiss()
-                        }
+            .navigationTitle("Add Journal Entry")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancle") {
+                        dismiss()
                     }
                 }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save") {
+                        let journalEntry = JournalEntry(rating: 3, entryTitle: entryTitle, entryBody: entryBody, latitude: nil, longitude: nil)
+                        dismiss()
+                    }
+                }
+            }
         }
     }
 }
