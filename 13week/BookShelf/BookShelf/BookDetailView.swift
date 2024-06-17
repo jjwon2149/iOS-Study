5cefd991b6374234d5976cf301949487f8640029
jjwon2149 <jjwon2149@gmail.com>
2024-06-17 17:41:51 +0900

    feat: add BookDetaiView

--- 13week/BookShelf/BookShelf/BookDetailView.swift
+++ 13week/BookShelf/BookShelf/BookDetailView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct BookDetailView: View {
     @Binding var book: Book
+    @State var showEditBookView = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Text(book.title)
+            Image(book.largeCoverImageName)
+                .resizable()
+                .scaledToFit()
+                .shadow(radius: 10)
+                .padding()
+            Label(book.author, systemImage: "person.crop.rectangle")
+            Label("ISBN: \(book.isbn)", systemImage: "number")
+            Label("\(book.pages) pages", systemImage: "book")
+            Toggle("Read", isOn: .constant(book.isRead))
+            Button(action: {
+                showEditBookView.toggle()
+            }, label: {
+                Label("Edit", systemImage: "pencil")
+            })
+        }
+        .navigationTitle(book.title)
     }
 }
 
 #Preview {
-    BookDetailView(book: .constant(Book(title: "test", author: "test", isbn: "test", pages: 100)))
+    NavigationStack {
+        BookDetailView(book: .constant(Book.sampleBooks[0]))
+    }
 }
