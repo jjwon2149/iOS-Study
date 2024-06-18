be24d3b52ae592e01c5fec5bcf2a032176fd0bd0
jjwon2149 <jjwon2149@gmail.com>
2024-06-18 09:37:03 +0900

    feat: checkISBN 추가 (작업중)

--- 13week/BookShelf/BookShelf/BookEditView.swift
+++ 13week/BookShelf/BookShelf/BookEditView.swift
@@ -9,14 +9,42 @@ import SwiftUI
 
 struct BookEditView: View {
     @Binding var book: Book
+    @State var isISBNValid = false
+    
+    
     var body: some View {
         Form {
             TextField("Book title", text: $book.title)
             Image(book.largeCoverImageName)
             TextField("Author", text: $book.author)
+            VStack(alignment: .leading) {
+                if !isISBNValid {
+                    Text("ISBN is inValid")
+                        .font(.caption)
+                        .foregroundStyle(Color.red)
+                }
+                TextField("ISBN", text: $book.isbn)
+            }
             TextField("Pages", value: $book.pages, formatter: NumberFormatter())
             Toggle("Read", isOn: $book.isRead)
         }
+        .onChange(of: book.isbn) { //개별뷰가 아닌 Form에 onChange
+            self.isISBNValid = checkISBN(isbn: book.isbn)
+        }
+    }
+    
+    func checkISBN(isbn: String) -> Bool {
+        guard !isbn.isEmpty else {
+            return false
+        }
+        
+        let sum = isbn
+            .compactMap { $0.wholeNumberValue }
+            .enumerated()
+            .map { $0.offset & 1 == 1 ? 3 * $0.element : $0.element }
+            .reduce(0, +)
+        
+        return sum % 10 == 0
     }
 }
 
