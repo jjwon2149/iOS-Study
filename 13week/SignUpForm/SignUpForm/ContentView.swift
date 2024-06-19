bc043f3ce4b6df7d938f3f15972a19837d2fa352
jjwon2149 <jjwon2149@gmail.com>
2024-06-19 10:14:29 +0900

    feat: add Closure 방식의 username 중복확인 코드

--- 13week/SignUpForm/SignUpForm/ContentView.swift
+++ 13week/SignUpForm/SignUpForm/ContentView.swift
@@ -17,6 +17,12 @@ class SignUpFormViewModel: ObservableObject {
     @Published var passwordMessage: String = ""
     @Published var isValid: Bool = false
     
+    @Published var isUserNameAvailable: Bool = false
+    
+    private let authenticationService = AuthenticationService()
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
     private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
         $username.map { $0.count >= 3 }.eraseToAnyPublisher()
     }()
@@ -44,7 +50,28 @@ class SignUpFormViewModel: ObservableObject {
             .eraseToAnyPublisher()
     }()
     
+    func checkUserNameAvailable(_ userName: String) {
+        authenticationService.checkUserNameAvailableWithClosure(userName: userName) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let isAvailable):
+                    self?.isUserNameAvailable = isAvailable
+                case .failure(let error):
+                    print("error: \(error)")
+                    self?.isUserNameAvailable = false
+                }
+            }
+        }
+    }
+    
     init() {
+        $username
+            .debounce(for: 0.5, scheduler: DispatchQueue.main) //username이 바뀌어도 속도 조절해서 서버에 request
+            .sink { [weak self] userName in
+                self?.checkUserNameAvailable(userName)
+            }
+            .store(in: &cancellables)
+        
         isFormValidPublisher.assign(to: &$isValid)
         isUsernameLengthValidPublisher.map { $0 ? "" : "Username must be at least three characters!"}
             .assign(to: &$usernameMessage)
