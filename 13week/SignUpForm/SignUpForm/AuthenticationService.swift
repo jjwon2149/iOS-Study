7e77a1882c8edbd3cdd6851c2b3c46cad47a8c30
Smith <jjwon2149@gmail.com>
2024-06-19 14:37:49 +0900

    feat: HTTP Status Code에 따른 오류메시지 처리

--- 13week/SignUpForm/SignUpForm/AuthenticationService.swift
+++ 13week/SignUpForm/SignUpForm/AuthenticationService.swift
@@ -13,6 +13,11 @@ struct UserNameAvailableMessage: Codable {
   var userName: String
 }
 
+struct APIErrorMessage: Decodable {
+    var error: Bool
+    var reason: String
+}
+
 enum NetworkError: Error {
   case transportError(Error)
   case serverError(statusCode: Int)
@@ -23,6 +28,10 @@ enum NetworkError: Error {
 
 enum APIError: LocalizedError {
     case invalidRequestError(String)
+    case transportError(Error)
+    case invalidResponse
+    case validationError(String)
+    case decodingError(Error)
 }
 
 class AuthenticationService {
@@ -33,6 +42,29 @@ class AuthenticationService {
         }
         
         return URLSession.shared.dataTaskPublisher(for: url)
+            .mapError { error -> Error in
+                return APIError.transportError(error)
+            }
+            .tryMap { (data, response) -> (data: Data, response: URLResponse) in
+                print("Received response from server, now checking status code")
+                guard let urlResponse = response as? HTTPURLResponse else {
+                    throw APIError.invalidResponse
+                }
+                if (200..<300) ~= urlResponse.statusCode {} else {
+                    let decoder = JSONDecoder()
+                    let apiError = try decoder.decode(APIErrorMessage.self, from: data)
+                    
+                    if urlResponse.statusCode == 400 {
+                        throw APIError.validationError(apiError.reason)
+                    }
+                    
+                    if (500..<600) ~= urlResponse.statusCode {
+                        let retryAfter = urlResponse.value(forHTTPHeaderField: "Retry-After")
+                        // TODO: SERVER ERROR HANDLING
+                    }
+                }
+                return (data, response)
+            }
             .map(\.data)
             .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
             .map(\.isAvailable)
