9c9e958f0f19e074f0884cb399a95f7277b2e733
Smith <jjwon2149@gmail.com>
2024-06-20 13:12:29 +0900

    feat: Add 비동기 퍼블리셔, 초기화 함수

--- 13week/SignupForm2/SignupForm2/SignupViewModel.swift
+++ 13week/SignupForm2/SignupForm2/SignupViewModel.swift
@@ -22,13 +22,27 @@ class SignupViewModel: ObservableObject {
     private lazy var isUsernameAvailablePublisher: AnyPublisher<Available, Never> = {
         $username
             .debounce(for: 0.5, scheduler: RunLoop.main) //시간 지연해서
-            .removeDuplicates()
+            .removeDuplicates() // 연속된 동일한 값의 변화를 무시
             .flatMap { username -> AnyPublisher<Available, Never> in //또다른 퍼블리셔를 사용하기 위해 flatmap
                 self.authenticationService.checkUserNameAvailablePublisher(userName: username)
                     .asResult()
             }
-            .share()
             .receive(on: DispatchQueue.main)// 메인 스레드에서
+            .print("before share")
+            .share()
+            .print(" share")
             .eraseToAnyPublisher()
     }()
+    
+    init() {
+        isUsernameAvailablePublisher.map { result in
+            switch result {
+            case .success(let isAvailable):
+                return isAvailable
+            case .failure(_):
+                return false
+            }
+        }
+        .assign(to: &$isValid)// 매핑된 결과를 isValid에 할당
+    }
 }
