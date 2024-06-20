d204beedb0350134963a948ddae8a38e8c12b889
Smith <jjwon2149@gmail.com>
2024-06-20 09:10:01 +0900

    Initialize Signup2 Project

---
+++ 13week/SignupForm2/SignupForm2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SignupForm2
+//
+//  Created by 정종원 on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 13week/SignupForm2/SignupForm2/SignupForm2App.swift
@@ -0,0 +1,17 @@
+//
+//  SignupForm2App.swift
+//  SignupForm2
+//
+//  Created by 정종원 on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SignupForm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
