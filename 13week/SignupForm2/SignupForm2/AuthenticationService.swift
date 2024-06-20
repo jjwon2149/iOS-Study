244683f4cd6717e5b0408f70809a839bcb7866e6
Smith <jjwon2149@gmail.com>
2024-06-20 11:12:32 +0900

    feat: add AuthenticationService (컴바인 적용중)

--- 13week/SignupForm2/SignupForm2/AuthenticationService.swift
+++ 13week/SignupForm2/SignupForm2/AuthenticationService.swift
@@ -9,8 +9,17 @@ import Foundation
 import Combine
 
 struct AuthenticationService {
-    //swift vapor로 만든 서버에서 유저이름이 맞는지 확인하는 메서드
+    //swift vapor로 만든 서버에서 유저이름이 맞는지 확인하는 메서드 (컴바인 활용
     func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
-        return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+        
+        guard let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)") else { return Fail(error: APIError.invalidResponse)
+                .eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data) //Result의 data정보
+            .decode(type: UserNameAvailableMessage.self, decoder: JSONDecoder())
+            .map(\.isAvailable) //decode된 정보에서 isAvailable만 뽑기
+            .eraseToAnyPublisher()
     }
 }
