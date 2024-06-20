564ea88c8c8d190173633cb939f32a2983b166bf
Smith <jjwon2149@gmail.com>
2024-06-20 09:46:54 +0900

    add AuthenticationService swift vapor로 만든 서버에서 유저이름이 맞는지 확인하는 메서드, Helper Methods

--- 13week/SignupForm2/SignupForm2/AuthenticationService.swift
+++ 13week/SignupForm2/SignupForm2/AuthenticationService.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import Combine
+
+struct AuthenticationService {
+    //swift vapor로 만든 서버에서 유저이름이 맞는지 확인하는 메서드
+    func checkUserNameAvailablePublisher(userName: String) -> AnyPublisher<Bool, Error> {
+        return Fail(error: APIError.invalidResponse).eraseToAnyPublisher()
+    }
+}
--- 13week/SignupForm2/SignupForm2/Helper.swift
+++ 13week/SignupForm2/SignupForm2/Helper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+struct UserNameAvailableMessage: Codable {
+    var isAvailable: Bool
+    var userName: String
+    //{key: value}
+}
+
+// 에러와 에러 메세지
+struct APIErrorMessage: Decodable {
+    var error: Bool
+    var reson: String
+}
+
+enum APIError: LocalizedError {
+    case invalidResponse
+}
+
+extension Publisher {
+    func asResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        //Result 객체는 success거나 failure임.
+        self.map(Result.success)
+            .catch { error in
+                Just(.failure(error))
+            }
+            .eraseToAnyPublisher() //게시
+    }
+}
