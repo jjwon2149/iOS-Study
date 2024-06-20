7b80d105c5b9f1366da0dcfb5de040dcefd7aacd
Smith <jjwon2149@gmail.com>
2024-06-20 13:18:03 +0900

    style: share() 설명 추가

--- 13week/SignupForm2/SignupForm2/SignupViewModel.swift
+++ 13week/SignupForm2/SignupForm2/SignupViewModel.swift
@@ -29,7 +29,7 @@ class SignupViewModel: ObservableObject {
             }
             .receive(on: DispatchQueue.main)// 메인 스레드에서
             .print("before share")
-            .share()
+            .share() //cold observable을 단 한번만 방출시키게 하는 것 cold observable을 단 한번만 방출하게하여 observable 내부가 중복으로 실행되지 않게끔하기 위함 /cold observable : 이벤트를 발행할때 observer가 구독했을때 방출하는 것 (observer가 구독하기 전까지는 방출을 안한다고해서 cold라고 표현) 각각의 observer마다 독립적인 데이터 생성하여 발행 ex) just, single, create
             .print(" share")
             .eraseToAnyPublisher()
     }()
