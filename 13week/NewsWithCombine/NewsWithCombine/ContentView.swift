c94ec316634344813227f89dcd216bf99891bbcd
Smith <jjwon2149@gmail.com>
2024-06-21 15:16:25 +0900

    Initialize NewsApp Project

---
+++ 13week/NewsWithCombine/NewsWithCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsWithCombine
+//
+//  Created by 정종원 on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 13week/NewsWithCombine/NewsWithCombine/NewsWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWithCombineApp.swift
+//  NewsWithCombine
+//
+//  Created by 정종원 on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
