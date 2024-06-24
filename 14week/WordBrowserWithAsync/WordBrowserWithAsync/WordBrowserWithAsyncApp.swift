36603532995971dc0170bee50cc05b8967c9484d
Smith <jjwon2149@gmail.com>
2024-06-24 13:11:57 +0900

    Initialize WordBrowserWithAsync Project

---
+++ 14week/WordBrowserWithAsync/WordBrowserWithAsync/WordBrowserWithAsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBrowserWithAsyncApp.swift
+//  WordBrowserWithAsync
+//
+//  Created by 정종원 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WordBrowserWithAsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
