48ef0ac4f715527cb6ba6f3683e7fc4eb028a1f0
Smith <jjwon2149@gmail.com>
2024-06-24 17:06:24 +0900

    feat: add WordDetailView Component, Properties

--- 14week/WordBrowserWithAsync/WordBrowserWithAsync/WordDetailView.swift
+++ 14week/WordBrowserWithAsync/WordBrowserWithAsync/WordDetailView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct WordDetailView: View {
+    @State var word: String
+    @StateObject var viewModel = WordDetailViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if viewModel.isSearching {
+                ProgressView("Fetching...")
+            } else {
+                List {
+                    Section("Definitions") {
+                        ForEach(viewModel.definitions) { definition in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text("(\(definition.partOfSpeech))")
+                                        .font(.caption)
+                                    Text(definition.definition)
+                                }
+                                Spacer()
+                            }
+                        }
+                    }
+                    .lineLimit(2)
+                }
+            }
+        }
+        .navigationTitle(word)
     }
 }
 
 #Preview {
-    WordDetailView()
+    NavigationStack {
+        WordDetailView(word: "Swift")
+    }
 }
