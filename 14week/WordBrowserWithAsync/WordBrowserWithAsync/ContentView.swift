89e17d526c272b03edbd0e02feb38a2c97a5f2da
Smith <jjwon2149@gmail.com>
2024-06-24 13:44:30 +0900

    feat: add initial View,  LibraryViewModel

--- 14week/WordBrowserWithAsync/WordBrowserWithAsync/ContentView.swift
+++ 14week/WordBrowserWithAsync/WordBrowserWithAsync/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = LibraryViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        List {
+            SectionView(title: "Random word", words: [viewModel.randomWord])
+            SectionView(title: "Peter's Tips", words: viewModel.filteredTips)
+            SectionView(title: "My Favorites", words: viewModel.filteredFavorites)
+        } //List
+        .searchable(text: $viewModel.searchText)
+        .textInputAutocapitalization(.never)
+        .navigationTitle("Library")
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
         }
-        .padding()
+    }
+}
+
+struct SectionView: View {
+    var title: String
+    var words: [String]
+    
+    init(title: String, words: [String]) {
+        self.title = title
+        self.words = words
+    }
+    
+    var body: some View {
+        Section(title) {
+            if words.isEmpty {
+                Text("No items match your filter criteria")
+            } else {
+                ForEach(words, id: \.self) { word in
+                    Text(word)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
---
+++ 14week/WordBrowserWithAsync/WordBrowserWithAsync/LibraryViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryViewModel.swift
+//  WordBrowserWithAsync
+//
+//  Created by 정종원 on 6/24/24.
+//
+
+import Foundation
+
+class LibraryViewModel: ObservableObject {
+    @Published var randomWord = "partially"
+    @Published var searchText = ""
+    @Published var tips: [String] = ["Swift", "authentication", "authorization"]
+    @Published var favorites: [String] = ["stunning", "brilliant", "marvelous"]
+    @Published var filteredTips = [String]()
+    @Published var filteredFavorites = [String]()
+}
