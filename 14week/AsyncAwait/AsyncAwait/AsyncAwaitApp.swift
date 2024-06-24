a9e203d10937df1c286bc969a45ff515a6761bf5
Smith <jjwon2149@gmail.com>
2024-06-24 09:41:11 +0900

    Initialize AsyncAwait Project

---
+++ 14week/AsyncAwait/AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by 정종원 on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
