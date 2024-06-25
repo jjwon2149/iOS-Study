1523578fc86ed2aebe607b8ecc3f4b1374e17e6a
Smith <jjwon2149@gmail.com>
2024-06-25 13:04:57 +0900

    Initial commit: Create WeatherKit Project

---
+++ 14week/WeatherKitProject/WeatherKitProject/WeatherKitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitProjectApp.swift
+//  WeatherKitProject
+//
+//  Created by 정종원 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
