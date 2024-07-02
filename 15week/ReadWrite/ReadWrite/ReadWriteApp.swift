5ed933eedc1ec45caad0c34fcac072434ba1ccf7
Smith <jjwon2149@gmail.com>
2024-07-02 10:05:34 +0900

    Initialize ReadWriteApp

---
+++ 15week/ReadWrite/ReadWrite/ReadWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWriteApp.swift
+//  ReadWrite
+//
+//  Created by 정종원 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
