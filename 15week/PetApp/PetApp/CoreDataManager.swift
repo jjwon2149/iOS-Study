944aa966d538e64054ec98e64351186b2586a795
Smith <jjwon2149@gmail.com>
2024-07-02 13:34:26 +0900

    feat: Create CoreDataManager

---
+++ 15week/PetApp/PetApp/CoreDataManager.swift
@@ -0,0 +1,56 @@
+//
+//  CoreDataManager.swift
+//  PetApp
+//
+//  Created by 정종원 on 7/2/24.
+//
+
+import CoreData
+
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "DataModel")
+        persistentContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Core Data Initialize Failed error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // Create
+    func savePet(name: String, breed: String) {
+        let pet = Animal(context: persistentContainer.viewContext)
+        pet.name = name
+        pet.breed = breed
+        do {
+            try persistentContainer.viewContext.save()
+            print("pet saved!")
+        } catch {
+            print("Failed to save \(error.localizedDescription)")
+        }
+    }
+    
+    // Read
+    func getAllPets() -> [Animal] {
+        let fetchRequest: NSFetchRequest<Animal> = Animal.fetchRequest()
+        
+        do {
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            return []
+        }
+    }
+    
+    // Delete
+    func deletePet(animal: Animal) {
+        persistentContainer.viewContext.delete(animal)
+        do {
+            try persistentContainer.viewContext.save()
+        } catch {
+            persistentContainer.viewContext.rollback()
+            print("Failed to save Error \(error.localizedDescription)")
+        }
+    }
+}
