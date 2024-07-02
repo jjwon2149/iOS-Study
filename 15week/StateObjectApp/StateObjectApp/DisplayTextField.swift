a38c6f9c1078e06b0b1f2d7f27a7b5f80b130d1c
Smith <jjwon2149@gmail.com>
2024-07-02 16:24:21 +0900

    Initialize StateObjectApp Project

---
+++ 15week/StateObjectApp/StateObjectApp/DisplayTextField.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayTextField.swift
+//  StateObjectApp
+//
+//  Created by 정종원 on 7/2/24.
+//
+
+import SwiftUI
+
+struct DisplayTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisplayTextField()
+}
