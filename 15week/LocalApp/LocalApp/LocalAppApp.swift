4b3d0fb85d522e6a3e19977b7cf8c44721b178c8
Smith <jjwon2149@gmail.com>
2024-07-03 09:22:23 +0900

    Initialize LocalApp Project

---
+++ 15week/LocalApp/LocalApp/LocalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAppApp.swift
+//  LocalApp
+//
+//  Created by 정종원 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
