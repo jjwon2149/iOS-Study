86ff031b988f3ce142cd89c01f2e4a14f81b5045
Smith <jjwon2149@gmail.com>
2024-07-03 11:05:51 +0900

    Initialize PsudoLanguageApp Project

---
+++ 15week/PsudoLanguageApp/PsudoLanguageApp/PsudoLanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PsudoLanguageAppApp.swift
+//  PsudoLanguageApp
+//
+//  Created by 정종원 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PsudoLanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
