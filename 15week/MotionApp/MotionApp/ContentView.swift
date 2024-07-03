ffc91991c67f024af7a46b291dd0c40b838efafa
Smith <jjwon2149@gmail.com>
2024-07-03 14:22:46 +0900

    feat: 가속도의 속도 변화량을 감지

--- 15week/MotionApp/MotionApp/ContentView.swift
+++ 15week/MotionApp/MotionApp/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
-
+import CoreMotion
+//가속도의 속도 변화량을 감지함
 struct ContentView: View {
+    
+    @State private var x: Double = 0.0
+    @State private var y: Double = 0.0
+    @State private var z: Double = 0.0
+    
+    let motionManager = CMMotionManager()
+    let queue = OperationQueue()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("x \(x)")
+            Text("y \(y)")
+            Text("z \(z)")
         }
         .padding()
+        .onAppear {
+            motionManager.startAccelerometerUpdates(to: queue) { (data: CMAccelerometerData?, error: Error?) in
+                guard let data = data else {
+                    print("error: \(error?.localizedDescription)")
+                    return
+                }
+                
+                let trackMotion: CMAcceleration = data.acceleration
+                motionManager.accelerometerUpdateInterval = 1 //센서가 예민해서 2.5초 주기로 설정
+                DispatchQueue.main.async {
+                    x = trackMotion.x
+                    y = trackMotion.y
+                    z = trackMotion.z
+                }
+            }
+        }
     }
 }
 
