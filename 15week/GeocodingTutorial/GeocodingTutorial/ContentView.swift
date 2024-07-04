8ef55ba16d5b548ddb939af8ffa93895e3c9fc24
Smith <jjwon2149@gmail.com>
2024-07-04 13:58:21 +0900

    feat: 현재위치 마커

--- 15week/GeocodingTutorial/GeocodingTutorial/ContentView.swift
+++ 15week/GeocodingTutorial/GeocodingTutorial/ContentView.swift
@@ -1,21 +1,67 @@
-//
-//  ContentView.swift
-//  GeocodingTutorial
-//
-//  Created by 정종원 on 7/4/24.
-//
-
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct ContentView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.785834, longitude: -122.406417), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    @State var searchText = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Map(coordinateRegion: $region,
+                showsUserLocation: true)
+            .onAppear {
+                if let mapRegion = locationManager.mapRegion {
+                    region = MKCoordinateRegion(center: mapRegion.center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+                    print(mapRegion.center)
+                }
+            }
+
+            
         }
         .padding()
+        .searchable(text: $searchText)
+
+    }
+}
+
+//MARK: - LocationManager
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+
+    @Published var mapRegion: MKCoordinateRegion? = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.457105, longitude: -80.508361), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    var binding: Binding<MKCoordinateRegion> {
+            Binding {
+                self.mapRegion ?? MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 43.457105, longitude: -80.508361), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+            } set: { newRegion in
+                self.mapRegion = newRegion
+            }
+        }
+
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+
+    }
+
+    func requestLocation() {
+        manager.requestLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first?.coordinate {
+            DispatchQueue.main.async {
+                self.mapRegion = MKCoordinateRegion(center: location, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            }
+        }
     }
 }
 
