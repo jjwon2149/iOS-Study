a6c252acdf89371a2bdc1c18d943a9af3af85e37
Smith <jjwon2149@gmail.com>
2024-07-04 13:07:51 +0900

    initialize Geocoding Project

---
+++ 15week/GeocodingTutorial/GeocodingTutorial/GeocodingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeocodingTutorialApp.swift
+//  GeocodingTutorial
+//
+//  Created by 정종원 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GeocodingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
