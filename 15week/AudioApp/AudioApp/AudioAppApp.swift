1f5c42f9682b8dbcaa217e8df2de1d46f98aa0ae
Smith <jjwon2149@gmail.com>
2024-07-05 10:08:52 +0900

    Initialize AudioApp Project

---
+++ 15week/AudioApp/AudioApp/AudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAppApp.swift
+//  AudioApp
+//
+//  Created by 정종원 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
