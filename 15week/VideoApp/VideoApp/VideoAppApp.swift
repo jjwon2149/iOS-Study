cc91f086cfa7feaa9bac7b3552c5bca4b1c31d85
Smith <jjwon2149@gmail.com>
2024-07-05 10:37:54 +0900

    Initialize VideoApp Project

---
+++ 15week/VideoApp/VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by 정종원 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
