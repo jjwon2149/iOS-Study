e7d06c171e5253fc310e1a183b044aec657547a2
Smith <jjwon2149@gmail.com>
2024-07-08 11:27:12 +0900

    Initialize PDFApp Project

---
+++ 16week/PDFApp/PDFApp/PDFAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFAppApp.swift
+//  PDFApp
+//
+//  Created by 정종원 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
