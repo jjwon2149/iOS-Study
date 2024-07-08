a48cdaa5af65b1d797ed755155e685312725faa1
Smith <jjwon2149@gmail.com>
2024-07-08 13:33:04 +0900

    Initialize UIKitWithCodeOnly (main삭제)

---
+++ 16week/UIKitWithCodeOnly/UIKitWithCodeOnly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitWithCodeOnly
+//
+//  Created by 정종원 on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
