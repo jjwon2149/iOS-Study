4928bb92cbf411e12adc2462e903b9de4202c4f6
Smith <jjwon2149@gmail.com>
2024-07-08 13:28:12 +0900

    UIKit에서 SwiftUI 연동

---
+++ 16week/StoryboardApp/StoryboardApp/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  StoryboardApp
+//
+//  Created by 정종원 on 7/8/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var name: String
+    var body: some View {
+        Text("Hello, \(name)")
+    }
+}
+
+#Preview {
+    SwiftUIView(name: "KOKO")
+}
