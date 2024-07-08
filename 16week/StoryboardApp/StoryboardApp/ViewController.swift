6568e7057e139d2f619a8b829d8923d0ba3974aa
Smith <jjwon2149@gmail.com>
2024-07-08 13:14:44 +0900

    Initialize StoryBoardApp Project

---
+++ 16week/StoryboardApp/StoryboardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardApp
+//
+//  Created by 정종원 on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
