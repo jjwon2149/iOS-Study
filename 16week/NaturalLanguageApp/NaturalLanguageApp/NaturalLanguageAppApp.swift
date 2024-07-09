26f691e14985b841c343ba8bc06e5aa374e02f50
Smith <jjwon2149@gmail.com>
2024-07-09 14:16:04 +0900

    Initialize NaturalLanguageApp Project

---
+++ 16week/NaturalLanguageApp/NaturalLanguageApp/NaturalLanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalLanguageAppApp.swift
+//  NaturalLanguageApp
+//
+//  Created by 정종원 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NaturalLanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
