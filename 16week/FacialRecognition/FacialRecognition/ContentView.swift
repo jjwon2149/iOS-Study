9f71f03d1c448f60f14b54ec14fbaf0b3fdfbd65
Smith <jjwon2149@gmail.com>
2024-07-09 10:23:23 +0900

    feat: 얼굴 강조 이미지 그리기

--- 16week/FacialRecognition/FacialRecognition/ContentView.swift
+++ 16week/FacialRecognition/FacialRecognition/ContentView.swift
@@ -15,13 +15,13 @@ struct ContentView: View {
     
     @State var message = ""
     @State var arrayIndex = 0
+    @State var newImage: UIImage = UIImage(systemName: "smiley.fill")!
     
     var body: some View {
         VStack {
             Image(photoArray[arrayIndex])
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 250, height: 250)
             Text(message)
                 .padding()
             Button {
@@ -31,6 +31,7 @@ struct ContentView: View {
             }.padding()
             
             HStack {
+                Spacer()
                 Button {
                     if arrayIndex == 0 {
                         arrayIndex = photoArray.count - 1
@@ -41,6 +42,7 @@ struct ContentView: View {
                 } label: {
                     Image(systemName: "chevron.left.square.fill")
                 }
+                Spacer()
                 Button {
                     if arrayIndex == photoArray.count - 1 {
                         arrayIndex = 0
@@ -51,6 +53,7 @@ struct ContentView: View {
                 } label: {
                     Image(systemName: "chevron.right.square.fill")
                 }
+                Spacer()
             }
         }
         .padding()
@@ -58,7 +61,7 @@ struct ContentView: View {
     
     func analyzeImage(image: UIImage) {
         let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
-        let request = VNDetectFaceRectanglesRequest(completionHandler: handleFaceRecognition)
+        let request = VNDetectFaceLandmarksRequest(completionHandler: handleFaceRecognition)
         try! handler.perform([request])
     }
     
@@ -67,6 +70,34 @@ struct ContentView: View {
             fatalError("Can't find a face in the picture")
         }
         message = "Found \(foundFaces.count) faces in the picture"
+        for faceRectangle in foundFaces {
+            let landmarkRegions: [VNFaceLandmarkRegion2D] = []
+            drawImage(source: newImage, boundary: faceRectangle.boundingBox, faceLandmarkRegions: landmarkRegions)
+        }
+    }
+    
+    func drawImage(source: UIImage, boundary: CGRect, faceLandmarkRegions: [VNFaceLandmarkRegion2D]) {
+        UIGraphicsBeginImageContextWithOptions(source.size, false, 1)
+        let context = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: 0, y: source.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setLineJoin(.round)
+        context.setLineCap(.round)
+        context.setShouldAntialias(true)
+        context.setAllowsAntialiasing(true)
+        let rect = CGRect(x: 0, y: 0, width: source.size.width, height: source.size.height)
+        context.draw(source.cgImage!, in: rect)
+        // 얼굴 주위에 직사각형을 그립니다.
+        let fillColor = UIColor.red
+        fillColor.setStroke()
+        let rectangleWidth = source.size.width * boundary.size.width
+        let rectangleHeight = source.size.height * boundary.size.height
+        context.setLineWidth(8)
+        context.addRect(CGRect(x: boundary.origin.x * source.size.width, y: boundary.origin.y * source.size.height, width: rectangleWidth, height: rectangleHeight))
+        context.drawPath(using: .stroke)
+        let modifiedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        newImage = modifiedImage
     }
 }
 
