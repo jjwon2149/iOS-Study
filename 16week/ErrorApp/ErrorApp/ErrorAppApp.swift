62ca7153df045a301fde496fe314565a4ba7eb77
Smith <jjwon2149@gmail.com>
2024-07-09 15:23:11 +0900

    Initialize ErrorApp Project

---
+++ 16week/ErrorApp/ErrorApp/ErrorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorAppApp.swift
+//  ErrorApp
+//
+//  Created by 정종원 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ErrorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
