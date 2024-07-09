eec69598d8b6f11ad39cd8fa5c0cddb63d644b4e
Smith <jjwon2149@gmail.com>
2024-07-09 15:11:12 +0900

    feat: guard를 사용한 오류처리

--- 16week/GuardApp/GuardApp/ContentView.swift
+++ 16week/GuardApp/GuardApp/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                 .scaledToFit()
                 .frame(width: 300, height: 400)
             Button {
-                getImage()
+                getImage("zzz") //없는 이미지 파일이 들어갈 경우
             } label: {
                 Text("Add Picture")
             }
@@ -26,8 +26,20 @@ struct ContentView: View {
         .padding()
     }
 
-    func getImage() {
-       showImage = UIImage(named: "car")!
+    func getImage(_ imageName: String) {
+//        //전통적인 방식
+//        if UIImage(named: imageName) != nil {
+//            showImage = UIImage(named: imageName)!
+//        } else {
+//            showImage = UIImage(systemName: "xmark.octagon.fill")!
+//        }
+        
+        //guard 사용
+        guard UIImage(named: imageName) != nil else {
+            showImage = UIImage(systemName: "xmark.octagon.fill")!
+            return
+        }
+        showImage = UIImage(named: imageName)!
     }
 }
 
