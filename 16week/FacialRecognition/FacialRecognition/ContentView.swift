56298afb98808aa085bff9522cec0745f3d1c8c7
Smith <jjwon2149@gmail.com>
2024-07-09 10:33:28 +0900

    style: drawImage 설명 추가

--- 16week/FacialRecognition/FacialRecognition/ContentView.swift
+++ 16week/FacialRecognition/FacialRecognition/ContentView.swift
@@ -77,17 +77,27 @@ struct ContentView: View {
     }
     
     func drawImage(source: UIImage, boundary: CGRect, faceLandmarkRegions: [VNFaceLandmarkRegion2D]) {
+        // 1. 새로운 그래픽 컨텍스트 시작
         UIGraphicsBeginImageContextWithOptions(source.size, false, 1)
+        
+        // 2. 현재 그래픽 컨텍스트 가져오기
         let context = UIGraphicsGetCurrentContext()!
+        
+        // 3. 좌표 변환 설정
         context.translateBy(x: 0, y: source.size.height)
         context.scaleBy(x: 1.0, y: -1.0)
+        
+        // 4. 그래픽 컨텍스트 설정
         context.setLineJoin(.round)
         context.setLineCap(.round)
         context.setShouldAntialias(true)
         context.setAllowsAntialiasing(true)
+        
+        // 5. 원본 이미지를 컨텍스트에 그림
         let rect = CGRect(x: 0, y: 0, width: source.size.width, height: source.size.height)
         context.draw(source.cgImage!, in: rect)
-        // 얼굴 주위에 직사각형을 그립니다.
+        
+        // 6. 얼굴 주위에 직사각형을 그림
         let fillColor = UIColor.red
         fillColor.setStroke()
         let rectangleWidth = source.size.width * boundary.size.width
@@ -95,8 +105,14 @@ struct ContentView: View {
         context.setLineWidth(8)
         context.addRect(CGRect(x: boundary.origin.x * source.size.width, y: boundary.origin.y * source.size.height, width: rectangleWidth, height: rectangleHeight))
         context.drawPath(using: .stroke)
+        
+        // 7. 새롭게 생성된 이미지를 가져옴
         let modifiedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        // 8. 그래픽 컨텍스트 종료
         UIGraphicsEndImageContext()
+        
+        // 9. 새로운 이미지 설정
         newImage = modifiedImage
     }
 }
