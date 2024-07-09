cbed3316170a060b61e56dd4319c2026c6af5765
Smith <jjwon2149@gmail.com>
2024-07-09 09:18:04 +0900

    Initialize FacialRecognition Project

---
+++ 16week/FacialRecognition/FacialRecognition/FacialRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognitionApp.swift
+//  FacialRecognition
+//
+//  Created by 정종원 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
