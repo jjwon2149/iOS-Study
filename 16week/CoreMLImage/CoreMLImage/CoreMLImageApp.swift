ffeb131f864da9509ec2ae346f5559133cce27b8
Smith <jjwon2149@gmail.com>
2024-07-09 13:19:09 +0900

    Initialize CoreMLImage Project

---
+++ 16week/CoreMLImage/CoreMLImage/CoreMLImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLImageApp.swift
+//  CoreMLImage
+//
+//  Created by 정종원 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
