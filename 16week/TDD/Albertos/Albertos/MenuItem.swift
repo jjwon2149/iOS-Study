bbee69cee9eb9cf6454224e58aad3ef357963ecf
Smith <jjwon2149@gmail.com>
2024-07-10 13:21:31 +0900

    feat: MenuItem, MenuSection 구조체 추가

---
+++ 16week/TDD/Albertos/Albertos/MenuItem.swift
@@ -0,0 +1,12 @@
+//
+//  MenuItem.swift
+//  Albertos
+//
+//  Created by 정종원 on 7/10/24.
+//
+
+import Foundation
+
+struct MenuItem {
+    
+}
---
+++ 16week/TDD/Albertos/Albertos/MenuSection.swift
@@ -0,0 +1,16 @@
+//
+//  MenuSection.swift
+//  Albertos
+//
+//  Created by 정종원 on 7/10/24.
+//
+
+import Foundation
+
+struct MenuSection {
+    
+}
+
+func groupMenuByCategory(_ menu: [MenuItem]) -> [MenuSection] {
+    return []
+}
