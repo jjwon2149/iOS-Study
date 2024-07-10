f8b3cdf20d6ac268eea561cc0d7926104ed996e6
Smith <jjwon2149@gmail.com>
2024-07-10 13:13:24 +0900

    test: add tests func

---
+++ 16week/TDD/Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -0,0 +1,23 @@
+//
+//  AlbertosTests.swift
+//  AlbertosTests
+//
+//  Created by 정종원 on 7/10/24.
+//
+
+import XCTest
+@testable import Albertos
+
+final class MenuGroupingTests: XCTestCase {
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
+        
+    }
+    
+    func testMenuWithOneCategoryReturnOneSection() {
+        
+    }
+    
+    func testEmptyMenuReturnsEmptySections() {
+        
+    }
+}
