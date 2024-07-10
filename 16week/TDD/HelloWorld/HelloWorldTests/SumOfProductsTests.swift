75c578448a5b838fd84fb23626fa04978bdf24b9
Smith <jjwon2149@gmail.com>
2024-07-10 11:43:52 +0900

    test: SumOfProductsTests 완료

--- 16week/TDD/HelloWorld/HelloWorldTests/SumOfProductsTests.swift
+++ 16week/TDD/HelloWorld/HelloWorldTests/SumOfProductsTests.swift
@@ -7,6 +7,39 @@
 
 import XCTest
 
-final class SumOfProductsTests: XCTestCase {
+struct Product {
+    let category: String
+    let price: Double
+}
 
+func sumOf(_ products: [Product], withCategory category: String) -> Double {
+    return products
+        .filter { $0.category == category }
+        .reduce(0.0) { $0 + $1.price }
+}
+
+final class SumOfProductsTests: XCTestCase {
+    
+    func testSumOfEmptyArrayIsZero() {
+        let category = "books"
+        let products = [Product]()
+        let sum = sumOf(products, withCategory: category)
+        XCTAssertEqual(sum, 0)
+    }
+    func testSumOfOneItemIsItemPrice() {
+        let category = "books"
+        let products = [Product(category: category, price: 3)]
+        let sum = sumOf(products, withCategory: category)
+        XCTAssertEqual(sum, 3)
+    }
+    func testSumIsSumOfItemsProcesForGivenCategory() {
+        let category = "books"
+        let products = [
+            Product(category: category, price: 3),
+            Product(category: "movies", price: 2),
+            Product(category: category, price: 1)
+        ]
+        let sum = sumOf(products, withCategory: category)
+        XCTAssertEqual(sum, 4)
+    }
 }
