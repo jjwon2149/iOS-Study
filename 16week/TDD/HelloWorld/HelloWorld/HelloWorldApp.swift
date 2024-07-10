9557f947199e8487c4e827c9e15d02feff3bb2f3
Smith <jjwon2149@gmail.com>
2024-07-10 10:37:28 +0900

    Initialize HelloWorld Tests

---
+++ 16week/TDD/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 정종원 on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
