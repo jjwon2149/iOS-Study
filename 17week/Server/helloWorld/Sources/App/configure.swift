abe35fd361e26269813c5a309ab3e7c2953f5dd7
Smith <jjwon2149@gmail.com>
2024-07-16 10:05:40 +0900

    feat: 데이터베이스 Fluent 라이브러리 추가 & 설정

--- 17week/Server/helloWorld/Sources/App/configure.swift
+++ 17week/Server/helloWorld/Sources/App/configure.swift
@@ -3,6 +3,10 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     app.logger.logLevel = .debug
+    
+    // 데이터베이스 드라이버 로드
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
