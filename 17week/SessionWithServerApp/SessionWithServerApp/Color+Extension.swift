ef667b0c644083b497491de6aa24c6014de4ba20
Smith <jjwon2149@gmail.com>
2024-07-18 09:53:46 +0900

    feat: Add Color Extension

---
+++ 17week/SessionWithServerApp/SessionWithServerApp/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  SessionWithServerApp
+//
+//  Created by 정종원 on 7/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBackgroundColor = Color("customBackgroundColor")
+    static let customSNSLoginButtonColor = Color("customSNSLoginButtonColor")
+    static let customLoginHaveAnColor = Color("customLoginHaveAnColor")
+    static let customLoginButtonColor = Color("customLoginButtonColor")
+}
