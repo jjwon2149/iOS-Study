a748148638394c0c0de0139641d01f8378492b35
Smith <jjwon2149@gmail.com>
2024-07-18 09:20:39 +0900

    Initialize SessionWithServerApp

---
+++ 17week/SessionWithServerApp/SessionWithServerApp/SessionWithServerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionWithServerAppApp.swift
+//  SessionWithServerApp
+//
+//  Created by 정종원 on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SessionWithServerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
