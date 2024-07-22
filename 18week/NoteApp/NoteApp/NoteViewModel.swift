4e70414a3edfa3711345b64c2a549f17cf866279
Smith <jjwon2149@gmail.com>
2024-07-22 11:48:02 +0900

    feat: addNote,FormView,ContentView 추가

--- 18week/NoteApp/NoteApp/NoteViewModel.swift
+++ 18week/NoteApp/NoteApp/NoteViewModel.swift
@@ -11,4 +11,10 @@ import FirebaseFirestore
 class NoteViewModel: ObservableObject {
     @Published var notes = [Note]()
     
-    private var databaseReference = Firestore.firestore().collection("Notes")}
+    private var databaseReference = Firestore.firestore().collection("Notes")
+    func addData(title: String) {
+        let docRef = databaseReference.addDocument(data: ["title": title])
+        dump(docRef)
+    }
+}
+
