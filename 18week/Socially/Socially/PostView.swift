0b509e4c0f44afa27b5edeee5ad6fc6f7e73a52d
Smith <jjwon2149@gmail.com>
2024-07-23 14:20:06 +0900

    feat: PostView 추가

---
+++ 18week/Socially/Socially/PostView.swift
@@ -0,0 +1,41 @@
+//
+//  PostView.swift
+//  Socially
+//
+//  Created by 정종원 on 7/23/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    
+    @EnvironmentObject private var viewModel: PostViewModel
+    
+    @State private var description = ""
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Description", text: $description)
+                }
+                
+                Section {
+                    Button {
+                        Task {
+                            await self.viewModel.addData(description: description, datePublished: Date())
+                        }
+                    } label: {
+                        Text("Post")
+                    }
+                }
+            }
+            .navigationTitle("New Post")
+        }
+    }
+}
+
+#Preview {
+    PostView()
+        .environmentObject(PostViewModel())
+}
