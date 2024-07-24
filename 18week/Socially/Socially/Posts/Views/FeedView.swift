1d0e2c1278e4e7ee6f1467f14b2b2bd84a844010
Smith <jjwon2149@gmail.com>
2024-07-24 11:11:19 +0900

    chore: 폴더정리

--- 18week/Socially/Socially/Posts/Views/FeedView.swift
+++ 18week/Socially/Socially/Posts/Views/FeedView.swift
@@ -12,15 +12,30 @@ struct FeedView: View {
     
     @FirestoreQuery(collectionPath: "Posts") var posts: [Post]
     @EnvironmentObject private var authModel: AuthViewModel
-
+    
     var body: some View {
         NavigationStack {
             List(posts) { post in
                 VStack(alignment: .leading) {
-                    VStack {
+                    AsyncImage(url: URL(string: post.imageURL ?? "")) { phase in
+                        switch phase {
+                        case .empty:
+                            EmptyView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .frame(width: 300, height: 200)
+                        case .failure:
+                            Image(systemName: "photo")
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                    
+                    VStack(alignment: .leading) {
                         Text(post.description ?? "")
                             .font(.headline)
-                            .padding(12)
+                            .padding([.bottom, .top], 6)
                         Text("Published on the \(post.datePublished?.formatted() ?? "")")
                             .font(.caption)
                     }
@@ -33,11 +48,13 @@ struct FeedView: View {
                     Button {
                         authModel.signOut()
                     } label: {
-                        Text("Sign Out")
+                        Text("Sign out")
                     }
                 }
             }
+            
         }
+        
     }
 }
 
