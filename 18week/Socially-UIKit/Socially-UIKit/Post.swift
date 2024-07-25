0d38618191b197a9e0289b41bbf0ce4fae6b2dbf
Smith <jjwon2149@gmail.com>
2024-07-25 11:21:31 +0900

    feat: Firebase 초기화 코드 추가, Post 모델 파일 추가

---
+++ 18week/Socially-UIKit/Socially-UIKit/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Socially-UIKit
+//
+//  Created by 정종원 on 7/25/24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Post: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var description: String?
+    var imageURL: String?
+    @ServerTimestamp var datePublished: Date?
+}
