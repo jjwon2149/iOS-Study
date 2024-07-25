93c4c3a09cdba4f90160ad36f52aded8ffd45d98
Smith <jjwon2149@gmail.com>
2024-07-25 11:43:29 +0900

    feat: tableView, diffableDataSource 초기화 - ViewController 타이틀, 탭 아이템 설정

--- 18week/Socially-UIKit/Socially-UIKit/Post.swift
+++ 18week/Socially-UIKit/Socially-UIKit/Post.swift
@@ -8,7 +8,7 @@
 import Foundation
 import FirebaseFirestore
 
-struct Post: Identifiable, Decodable {
+struct Post: Identifiable, Decodable, Hashable {
     @DocumentID var id: String?
     var description: String?
     var imageURL: String?
--- 18week/Socially-UIKit/Socially-UIKit/ViewController.swift
+++ 18week/Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    private var dataSource: UITableViewDiffableDataSource<Section, Post>!
+    private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Feed"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "text.bubble"), tag: 0)
     }
-
+    
+    //MARK: - Methods
+    func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        view.addSubview(tableView)
+        tableView.register(UITableView.self, forCellReuseIdentifier: "postCell")
+        tableView.rowHeight = 280
+    }
+    
+    func configureDataSource() {
+        dataSource = UITableViewDiffableDataSource<Section, Post>(tableView: tableView) { (tableView, indexPath, item) in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "postCell")
+            
+            var config = cell?.defaultContentConfiguration()
+            
+            config?.text = item.description
+            
+            cell?.contentConfiguration = config
+            
+            return cell
+        }
+    }
 
 }
 
