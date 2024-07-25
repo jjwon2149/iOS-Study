20f0f09cbeca3a92b5974efec52791a930a82cdd
Smith <jjwon2149@gmail.com>
2024-07-25 09:44:36 +0900

    Initialize Socially-UIKit Project

---
+++ 18week/Socially-UIKit/Socially-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Socially-UIKit
+//
+//  Created by 정종원 on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
